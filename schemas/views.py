023aa89cf7d5fee97840e7a8b4c48c73fa2d5e01
Dmytro Stehantsev <stegan296@gmail.com>
2025-07-18 16:07:46 +0300

    add basic schema_list.html

--- schemas/views.py
+++ schemas/views.py
@@ -7,7 +7,7 @@ from schemas.models import Schema
 class SchemaListView(LoginRequiredMixin, ListView):
     model = Schema
     template_name = "schemas/schema_list.html"
-    context_object_name = "schema_list"
+    context_object_name = "schemas"
 
     def get_queryset(self):
         return Schema.objects.filter(user=self.request.user).order_by("-updated_at")
