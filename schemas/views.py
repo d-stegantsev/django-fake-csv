9217cdf113d624880299e6bacf58b5a1a3b1c321
Dmytro Stehantsev <stegan296@gmail.com>
2025-07-18 15:32:22 +0300

    add SchemaListView and base template

--- schemas/views.py
+++ schemas/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from schemas.models import Schema
+
+
+class SchemaListView(LoginRequiredMixin, ListView):
+    model = Schema
+    template_name = "schemas/schema_list.html"
+    context_object_name = "schema_list"
+
+    def get_queryset(self):
+        return Schema.objects.filter(user=self.request.user).order_by("-updated_at")
