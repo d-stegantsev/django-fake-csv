58e58889f093faa31c0803401b59938233d387e9
Dmytro Stehantsev <stegan296@gmail.com>
2025-07-30 11:04:40 +0300

    add basic SchemaDetailView + html + url

--- schemas/urls.py
+++ schemas/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SchemaListView, SchemaCreateView
+from .views import SchemaListView, SchemaCreateView, SchemaDetailView
 
 app_name = "schemas"
 
 urlpatterns = [
     path("", SchemaListView.as_view(), name="schema_list"),
     path("new/", SchemaCreateView.as_view(), name="schema_create"),
+    path("schemas/<int:pk>/", SchemaDetailView.as_view(), name="schema_detail"),
 ]
--- schemas/views.py
+++ schemas/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from schemas.forms import SchemaForm, SchemaColumnFormSet
 from schemas.models import Schema
@@ -40,3 +40,14 @@ class SchemaCreateView(LoginRequiredMixin, CreateView):
             return redirect(self.success_url)
 
         return render(request, self.template_name, {"form": form, "formset": formset})
+
+
+class SchemaDetailView(LoginRequiredMixin, DetailView):
+    model = Schema
+    template_name = "schemas/schema_detail.html"
+    context_object_name = "schema"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["columns"] = self.object.columns.all().order_by("order")
+        return context
