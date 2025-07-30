a5b317ef928d967fcbfae3dcf639569725a75908
Dmytro Stehantsev <stegan296@gmail.com>
2025-07-30 14:01:13 +0300

    add dotenv for settings

--- fakecsv/celery.py
+++ fakecsv/celery.py
@@ -1,6 +1,11 @@
 import os
 from celery import Celery
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fakecsv.settings")
 app = Celery("fakecsv")
 app.config_from_object("django.conf:settings", namespace="CELERY")
